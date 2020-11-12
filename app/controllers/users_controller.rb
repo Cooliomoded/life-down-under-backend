3700de3368edd31a1dd71554d06b6ad5b6b49a8b
noaheakin <noaheakin@gmail.com>
2020-11-12 10:15:53 -0800

    added show to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-    before_action :get_user, only: [:show, :edit, :update, :destroy]
+    before_action :get_user, only: [:edit, :update, :destroy]
 
     def index
         users = User.all
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
     end
 
     def show
+        user = User.find(params[:id])
+        render :json => user
     end
 
     def new
